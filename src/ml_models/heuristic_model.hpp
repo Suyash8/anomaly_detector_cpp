accdf43d88fcf46639ed21c20d7911e986feea87
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-06-16 17:10:06 +0530

    refactor(ml): Rename StubModel to HeuristicModel in preparation for new logic

--- src/ml_models/heuristic_model.hpp
+++ src/ml_models/heuristic_model.hpp
@@ -1,14 +1,14 @@
-#ifndef STUB_MODEL_HPP
-#define STUB_MODEL_HPP
+#ifndef HEURISTIC_MODEL_HPP
+#define HEURISTIC_MODEL_HPP
 
 #include "base_model.hpp"
 
 // A simple stub model for testing the pipeline before real models are
 // integrated
-class StubModel : public IAnomalyModel {
+class HeuristicModel : public IAnomalyModel {
 public:
   std::pair<double, std::vector<std::string>>
   score_with_explanation(const std::vector<double> &features) override;
 };
 
-#endif // STUB_MODEL_HPP
+#endif // HEURISTIC_MODEL_HPP
