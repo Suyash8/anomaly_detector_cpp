f75fc13d9788651e796d625c86c42bc62b7742b3
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-06-21 13:03:52 +0530

    feat(cache): Implement the 'put' operation for LRUCache

--- src/state/lru_cache.hpp
+++ src/state/lru_cache.hpp
@@ -24,4 +24,36 @@ private:
   size_t max_size_;
 };
 
+// --- Implementation ---
+
+template <typename Key, typename Value>
+
+LRUCache<Key, Value>::LRUCache(size_t max_size) : max_size_(max_size) {
+  if (max_size_ < 1)
+    max_size_ = 1;
+}
+
+template <typename Key, typename Value>
+void LRUCache<Key, Value>::put(const Key &key, Value value) {
+  auto it = items_map_.find(key);
+
+  // Case 1: Key already exists
+  if (it != items_map_.end()) {
+    it->second->second = std::move(value);
+    items_list_.splice(items_list_.begin(), items_list_, it->second);
+    return;
+  }
+
+  // Case 2: Key does not exist, and cache is full
+  if (items_map_.size() >= max_size_) {
+    Key lru_key = items_list_.back().first;
+    items_map_.erase(lru_key);
+    items_list_.pop_back();
+  }
+
+  // Case 3: Key does not exist, add it
+  items_list_.emplace_front(key, std::move(value));
+  items_map_[key] = items_list_.begin();
+}
+
 #endif // LRU_CACHE_HPP
