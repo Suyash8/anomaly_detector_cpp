23dec20e26a9a6d7219116485b0fd6d6639904ce
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-06-21 14:23:34 +0530

    feat(persistence): Implement key-to-shard mapping logic

--- src/state/persistence_manager.cpp
+++ src/state/persistence_manager.cpp
@@ -1,6 +1,8 @@
 #include "persistence_manager.hpp"
 #include <cstddef>
+#include <functional>
 #include <optional>
+#include <string>
 
 PersistenceManager::PersistenceManager(std::string base_dir, size_t num_shards)
     : base_dir_(std::move(base_dir)), num_shards_(num_shards) {}
@@ -17,5 +19,11 @@ PersistenceManager::read_state(const std::string &key) {
 
 std::string
 PersistenceManager::get_shard_path_for_key(const std::string &key) const {
-  return "";
+  if (num_shards_ == 0)
+    return "";
+
+  std::hash<std::string> hasher;
+  size_t shard_index = hasher(key) % num_shards_;
+
+  return base_dir_ + "/shard_" + std::to_string(shard_index) + ".db";
 }
