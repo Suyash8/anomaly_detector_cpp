6648d09bd93593f8dc495e97cc541efe991c4dc3
Suyash Sharma <sharmasuyash821+cc@gmail.com>
2025-07-14 11:53:50 +0530

    feat(state): Add default limits and duration to PerIpState and PerSessionState, and implement count retrieval methods

--- src/analysis/per_session_state.hpp
+++ src/analysis/per_session_state.hpp
@@ -11,6 +11,9 @@
 #include <unordered_set>
 
 struct PerSessionState {
+  int default_elements_limit = 200;
+  int default_duration_ms = 60000; // 60 seconds
+
   uint64_t session_start_timestamp_ms = 0;
   uint64_t last_seen_timestamp_ms = 0;
 
@@ -35,17 +38,26 @@ struct PerSessionState {
   // --- Window for High-Frequency Activity Detection ---
   SlidingWindow<uint64_t> request_timestamps_window;
 
+  size_t get_request_timestamps_count() const {
+    return request_timestamps_window.get_event_count();
+  }
+  size_t get_unique_paths_count() const { return unique_paths_visited.size(); }
+  size_t get_unique_user_agents_count() const {
+    return unique_user_agents.size();
+  }
+
   PerSessionState()
       : session_start_timestamp_ms(0), last_seen_timestamp_ms(0),
         failed_login_attempts(0), error_4xx_count(0), error_5xx_count(0),
         request_time_tracker(), bytes_sent_tracker(),
-        request_timestamps_window(0, 0) {}
+        request_timestamps_window(default_duration_ms, default_elements_limit) {
+  }
 
   // Constructor to initialize with the first event and window duration
   PerSessionState(uint64_t timestamp_ms, uint64_t window_duration_ms)
       : session_start_timestamp_ms(timestamp_ms),
         last_seen_timestamp_ms(timestamp_ms),
-        request_timestamps_window(window_duration_ms, 0) {}
+        request_timestamps_window(window_duration_ms, default_elements_limit) {}
 };
 
 #endif // PER_SESSION_STATE_HPP
